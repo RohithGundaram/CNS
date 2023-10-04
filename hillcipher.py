6a1b43cfff86a7cb16c8b009a13e2ebf6ac95051
Rohith Gundaram <87968757+RohithGundaram@users.noreply.github.com>
2023-10-04 10:29:21 +0530

    Update hillcipher.py

--- hillcipher.py
+++ hillcipher.py
@@ -14,8 +14,7 @@ def encrypt(messageVector):
 		for j in range(1):
 			cipherMatrix[i][j] = 0
 			for x in range(3):
-				cipherMatrix[i][j] += (keyMatrix[i][x] *
-									messageVector[x][j])
+				cipherMatrix[i][j] += (keyMatrix[i][x] *messageVector[x][j])
 			cipherMatrix[i][j] = cipherMatrix[i][j] % 26
 
 def HillCipher(message, key):
